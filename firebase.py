5e6b02852b40b0ccc58e47aba593bf2dfed87dc6
tyutyu2012 <tyutyu2012@gmail.com>
2018-04-27 17:41:46 -0500

    change reg id

--- firebase.py
+++ firebase.py
@@ -7,7 +7,7 @@ push_service = FCMNotification(api_key="AAAAN6nlb4k:APA91bHxb_WUirlGQjXKhYGCyFgT
 
 # Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
 
-registration_id = "fd5K_0556zw:APA91bG0l9cEQnhzea1vuojoalDI1h-x6FXSdAKgodwqEqyLIn3DN1Jjxfsh86fB-N3S1urcGMEyaUtMFcr4bIaBzrTLiSZxZ8_uDajNTNPHGukqW4FlWcR_5lesgmoZTcf7E-CWBImR"
+registration_id = "ehjTfV661II:APA91bGCJpucn-TaK96DgWiQcCEbU1-erSaWAGE_lkTGyMWbEDFu6hMLHXXwFjbFIQ2HUAypQgZkMwIIBRIKbhJTYomOwyM_t9kYZS521CmYFl8C_vmCvFp1nrPfiU-Qzfn-GgBteIu5"
 message_title = "Hello"
 message_body = "FireMessage"
 result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
